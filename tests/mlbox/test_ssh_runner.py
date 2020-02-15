d08bbd561e79ee5f290047988a4eaa3c88b880ee
Sergey Serebryakov <serebryakov.sergey@gmail.com>
2020-02-15 22:46:47 +0000

    Adding missing __init__.py files. Fixing documentation errors for the SSH runner tests.

--- tests/mlbox/test_ssh_runner.py
+++ tests/mlbox/test_ssh_runner.py
@@ -7,23 +7,26 @@ class TestSSHRunner(TestCase):
   """
   Run me like this:
     ```bash
-    # Go to MLBOX root directory
+    # Setup passwordless login
+    ...
+
+    # Go to MLBOX root directory.
     cd mlbox
 
-    # export variables for this test
+    # Export variables for this test.
     export MLBOX_SHH_RUNNER_HOST=...
     export MLBOX_SSH_RUNNER_SHH_ARG=-o,StrictHostKeyChecking=no
 
     # Set python path
-    export PYTHONROOT=$(pwd):${PYTHONROOT}
+    export PYTHONPATH=$(pwd):${PYTHONPATH}
 
     # Run this test case
-    python -m unittest mlbox.tests.TestSSHRunner.test_remote_host_name
+    python -m unittest tests.mlbox.test_ssh_runner.TestSSHRunner.test_remote_host_name
     ```
   """
 
   def test_remote_host_name(self):
-    """ Test a bare metal script. """
+    """ Test a bare metal script on a remote machine. """
     ssh_runner = SSHRunner(platform_config={
       "ssh_args": os.environ.get('MLBOX_SSH_RUNNER_SHH_ARG', '').split(','),
       "host": os.environ.get('MLBOX_SHH_RUNNER_HOST', None)
