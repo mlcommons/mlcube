4f4cef4459be6ae2f697f194002e298dc2ea7d1b
Sergey Serebryakov <sergey.serebryakov@hpe.com>
2020-02-14 16:51:26 -0800

    Fixing unit tests for a nvidia-docker test.

--- tests/mlbox/test_local_runner.py
+++ tests/mlbox/test_local_runner.py
@@ -4,22 +4,19 @@ from mlbox.runners.local import LocalRunner
 
 class TestLocalRunner(TestCase):
 
-  def test_run_bare_metal(self):
+  def run_bare_metal(self):
+    """ Test a bare metal script. """
     runner = LocalRunner(platform_config={})
     ret_code = runner.execute(['df', '-h'])
     self.assertEqual(0, ret_code)
 
-  def test_run_in_container_v01(self):
-    """
-    nvidia-docker run --rm -ti nvcr.io/nvidia/tensorflow:18.07-py3
-    cd /workspace/nvidia-examples/cnn
-    mpiexec --allow-run-as-root --bind-to socket -np 1 python resnet.py --precision=fp32 --log_dir=/tmp/nvtfcnn --batch_size=2 --num_iter=10 --iter_unit=batch --model=resnet50 --layers=50
-    """
-    # '/bin/bash -c \"cd /workspace/nvidia-examples/cnn && mpiexec --allow-run-as-root --bind-to socket -np 1 python resnet.py --precision=fp32 --log_dir=/tmp/nvtfcnn --batch_size=2 --num_iter=10 --iter_unit=batch --layers=50\"'
-    # 'mpiexec --allow-run-as-root --bind-to socket -np 1 python /workspace/nvidia-examples/cnn/resnet.py --precision=fp32 --log_dir=/tmp/nvtfcnn --batch_size=2 --num_iter=10 --iter_unit=batch --layers=50'
-    # '/bin/bash -c "echo Host name inside container $(hostname)"'
+  def test_run_in_container(self):
+    """ Test a script inside a container. """
     runner = LocalRunner(platform_config={})
-    runner.execute([
-      'nvidia-docker', 'run', '--rm', '-i', 'nvcr.io/nvidia/tensorflow:18.07-py3', 'hostname'
+    ret_code = runner.execute([
+      'nvidia-docker', 'run', '--rm', '-i', 'nvcr.io/nvidia/tensorflow:18.07-py3', '/bin/bash', '-c',
+      'cd /workspace/nvidia-examples/cnn && mpiexec --allow-run-as-root --bind-to socket -np 1 ' +
+      'python resnet.py --precision=fp32 --log_dir=/tmp/nvtfcnn ' +
+      '--batch_size=2 --num_iter=10 --iter_unit=batch --layers=50'
     ])
-    # self.assertEqual(0, ret_code)
+    self.assertEqual(0, ret_code)
