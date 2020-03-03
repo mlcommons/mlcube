7e9d93da3fa41f4fa98475ce39fafbe28277b9f3
Sergey Serebryakov <sergey.serebryakov@hpe.com>
2020-03-02 17:15:09 -0800

    Working MNIST example.

--- examples/mnist/README.md
+++ examples/mnist/README.md
@@ -1,3 +1,12 @@
 # MNIST MLBox example
- 
- 
+
+An MNIST MLBox example that is compatible with `mlbox_local_run.py` runner.
+
+MLPerf implements two tasks - `downloaddata` and `train`:
+```bash
+# Download data
+python ./mlbox_local_run.py ../examples/mnist:download/default
+
+# Train MNIST model. 
+python ./mlbox_local_run.py ../examples/mnist:train/default
+```
--- test_runner.py
+++ test_runner.py
@@ -10,67 +10,93 @@ Simulates the following functionality:
 import sys
 import yaml
 import os
-
 import logging
+import subprocess
+from typing import List
 
 # noinspection PyArgumentList
-logging.basicConfig(
-    level=logging.INFO,
-    format="[MLBOX] %(message)s",
-    handlers=[
-        logging.StreamHandler()
-    ]
-)
-
-from mlbox.runners.local import LocalRunner
+logging.basicConfig(level=logging.INFO, format="[MLBOX] %(message)s", handlers=[logging.StreamHandler()])
 
 
 def load_config(file_path: str) -> dict:
-  with open(file_path, 'r') as config_stream:
-      return yaml.load(config_stream, Loader=yaml.FullLoader)
+    with open(file_path, 'r') as config_stream:
+        return yaml.load(config_stream, Loader=yaml.FullLoader)
+
+
+def get_task_parameters(task: str, config: dict) -> List[str]:
+    task_spec = config['tasks'][task]
+    return list(task_spec['inputs'].keys()) + list(task_spec['outputs'].keys())
 
 
 def main():
-  # Get command line arguments
-  action, target = sys.argv[1], sys.argv[2]
-  if action == 'configure':
-    mlbox_path = sys.argv[3]
-  else:
-    task = sys.argv[3]
-    mlbox_path = sys.argv[4]
+    """
+    test_runner [configure|run] [runner] mlbox ...
 
-  # Load configuration file
-  mlbox_path = os.path.abspath(mlbox_path)
-  config = load_config(os.path.join(mlbox_path, 'mlbox.yaml'))
-  # Temporary solution to pass path to runners
-  config['_mlbox_path'] = mlbox_path
-  config['mlbox_docker_info']['context_path'] = os.path.join(mlbox_path, 'internals')
-  if config['implementation'] != 'mlbox_docker':
-    raise ValueError("Unknown MLBox docker implementation ('{}')".format(config['implementation']))
+    test_runner configure local ./examples/mnist
+    test_runner run local ./examples/mnist:downloaddata/default
+    test_runner run local ./examples/mnist:train/default
+    """
+    action, runner, task = sys.argv[1], sys.argv[2], sys.argv[3]
 
-  # Add proxy info if found in the environment
-  for proxy in ('http_proxy', 'https_proxy'):
-    proxy_val = os.environ.get(proxy, os.environ.get(proxy.upper(), None))
-    if proxy_val is not None:
-      config['mlbox_docker_info']['build_args'].update({proxy: proxy_val})
-      config['mlbox_docker_info']['run_args'].extend(['-e', '{}={}'.format(proxy, proxy_val)])
+    #                       action    runner mlbox
+    # python test_runner.py configure local  ./examples/mnist
+    if action != "run":
+        raise NotImplemented("Only 'run' action is implemented")
+    if runner != "local":
+        raise NotImplemented("Only 'local' runner is implemented")
 
-  # Instantiate a runner
-  if target == 'local':
-    runner = LocalRunner(config)
-  else:
-    raise ValueError("Unknown runner ('{}')".format(target))
+    # Parse task: mlbox_path:task/parameters
+    task, param_set = task.rsplit('/', 1)
+    mlbox, task = task.rsplit(':', 1)
+    print("MLBox={}, task={}, parameter_set={}".format(mlbox, task, param_set))
 
-  # Execute task or configure
-  if action == 'configure':
-    runner.configure()
-  elif action == 'execute':
-    # For the sake of PoC, just take it from the configuration
-    dc = config['mlbox_docker_info']
-    cmd = [dc['docker'], 'run'] + dc['run_args'] + [dc['image']] + dc['run_cmd']
-    runner.execute(cmd)
+    # Load MLBox specs
+    mlbox_root_dir = os.path.abspath(mlbox)
+    config = load_config(os.path.join(mlbox_root_dir, 'mlbox.yaml'))
+
+    # Get task parameters
+    # TODO: This is not used. Why?
+    task_params = get_task_parameters(task, config)
+
+    # Load task parameter values. Each parameter is either a directory or a file.
+    task_param_values = load_config(os.path.join(os.path.abspath(mlbox), 'tasks', task, "{}.yaml".format(param_set)))
+
+    # Compute directory/file mappings for docker: --volume HOST_PATH:DOCKER_PATH. Naive implementation.
+    docker_args, mlbox_args = [], []
+    # The param_name and param_value are defined in task/default.yaml configuration file.
+    for param_name, host_path in task_param_values.items():
+        # If it's a workspace path, convert to absolute host path
+        if host_path.startswith('workspace/'):
+            host_path = os.path.join(mlbox_root_dir, host_path)
+        host_path = os.path.abspath(host_path)
+        # Always mount host folder in /mlbox_io inside container. The concrete folder name is the name of the
+        # parameter, for instance, /mlbox_io/data_dir or /mlbox_io/model_dir.
+        docker_args.append('--volume')
+        docker_path = "/mlbox_io/{}".format(param_name)
+        #
+        if os.path.isdir(host_path):
+            docker_args.append("{}:{}".format(host_path, docker_path))
+            mlbox_args.extend(["--{}".format(param_name), docker_path])
+        elif os.path.isfile(host_path):
+            host_path, file_name = host_path.rsplit('/', 1)
+            docker_args.append("{}:{}".format(host_path, docker_path))
+            mlbox_args.extend(["--{}".format(param_name), "{}/{}".format(docker_path, file_name)])
+        else:
+            raise ValueError("Wrong configuration parameter: {} = {}".format(param_name, host_path))
+
+    #
+    cmd = ["docker", "run"] + \
+          docker_args + \
+          ["--rm", "-i", "mlperf/mlbox-example-mnist", "/bin/bash", "-c",
+           "cd /workspace && python3 ./mnist.py {} {}".format(task, " ".join(mlbox_args))]
+    print(cmd)
+
+    try:
+        return subprocess.check_call(cmd, stdout=sys.stdout, stderr=sys.stderr)
+    except subprocess.CalledProcessError as err:
+        logging.warning("Error while executing MLBox: cmd=%s, err=%s", str(cmd), str(err))
+        return err.returncode
 
 
 if __name__ == '__main__':
-  main()
-
+    main()
