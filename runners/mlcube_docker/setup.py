bb8ca8e942a3a863d91f435d3c30349b3ad7e0a4
Subin M <subin@apache.org>
2020-11-20 17:04:53 -0500

    update version to 0.0.2

--- runners/mlcube_docker/setup.py
+++ runners/mlcube_docker/setup.py
@@ -38,7 +38,7 @@ extra_files = schemas()
 
 setup(
     name="mlcube_docker",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(exclude=["tests"]),
     license="Apache 2.0",
     entry_points='''
