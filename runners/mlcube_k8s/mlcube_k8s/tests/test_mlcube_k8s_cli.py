b89bd33f1a11074f98bed3f270eca899e4f96e64
Sergey Serebryakov <serebryakov.sergey@gmail.com>
2020-11-24 01:03:08 -0800

    Fixing unit tests.

--- runners/mlcube_k8s/mlcube_k8s/tests/test_mlcube_k8s_cli.py
+++ runners/mlcube_k8s/mlcube_k8s/tests/test_mlcube_k8s_cli.py
@@ -8,4 +8,4 @@ def test_mlcube_k8s():
     response = runner.invoke(cli)
     assert response.exit_code == 0
     assert 'Usage: mlcube_k8s [OPTIONS] COMMAND [ARGS]...' in response.output
-    assert "  run  Runs a MLCube in a Kubernetes cluster." in response.output
+    assert "Runs a MLCube in a Kubernetes cluster." in response.output
