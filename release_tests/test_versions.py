385468deb60ca1fdf31ea672e67af6a757ccfe29
Sergey Serebryakov <serebryakov.sergey@gmail.com>
2021-09-30 23:04:42 -0700

    Release tests update. (#208)
    - Adding doc strings.
    - Updating type annotations.
    - Adding more asserts for certain corner cases.

--- release_tests/test_versions.py
+++ release_tests/test_versions.py
@@ -1,23 +1,32 @@
 import subprocess
+import typing as t
 from pathlib import Path
-from typing import Optional
 from release_tests import (ReleaseTest, CurrentDir)
 
 
 class VersionsTest(ReleaseTest):
-    @staticmethod
-    def get_version(directory: Path) -> Optional[str]:
-        if not directory.joinpath('setup.py').exists():
-            return None
+    """ Test all projects use the same release version. """
+
+    def get_version(self, directory: Path) -> t.Optional[t.Text]:
+        """ Return project version specified in setup.py file. """
+        self.assertTrue(
+            directory.joinpath('setup.py').exists(),
+            f"The 'setup.py' file does not exist in {directory}"
+        )
         with CurrentDir(directory.resolve()):
             result = subprocess.run(['python', 'setup.py', '--version'], stdout=subprocess.PIPE)
-        return result.stdout.decode("utf-8").strip()
+        version = result.stdout.decode("utf-8").strip()
+        self.assertTrue(
+            version is not None and version != '',
+            f"Invalid version in setup.py (directory={directory}, version={version})."
+        )
+        return version
 
     def test_package_versions(self) -> None:
-        #
-        mlcube_ver = VersionsTest.get_version(self.mlcube_dir)
+        """ Test that all package versions are the same. """
+        mlcube_ver = self.get_version(self.mlcube_dir)
         for runner_dir in self.runner_dirs:
-            runner_ver = VersionsTest.get_version(runner_dir)
+            runner_ver = self.get_version(runner_dir)
             self.assertEqual(
                 mlcube_ver,
                 runner_ver,
@@ -26,7 +35,8 @@ class VersionsTest(ReleaseTest):
         print(f"Release version: {mlcube_ver}")
 
     def test_dependency_versions(self) -> None:
-        expected_ver = VersionsTest.get_version(self.mlcube_dir)
+        """ Test that all runners required current MLCube version. """
+        expected_ver = self.get_version(self.mlcube_dir)
         for path in self.runner_dirs:
             with open(path.joinpath('requirements.txt'), 'r') as requirements:
                 packages = [line.strip() for line in requirements]
