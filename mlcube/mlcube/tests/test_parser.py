da8338f389568347f2d2566806bb979b9a7d0ea6
Sergey Serebryakov <serebryakov.sergey@gmail.com>
2022-08-01 15:27:14 -0700

    New unit tests.
    New unit tests cover some of the methods for the following classes: `MLCubeConfig`, `IOType`, `ParameterType` and `CliParser`.

---
+++ mlcube/mlcube/tests/test_parser.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+from omegaconf import (DictConfig, OmegaConf)
+from mlcube.parser import CliParser
+
+
+class TestParser(TestCase):
+    def test_parse_list_arg(self) -> None:
+        for arg in ("", None):
+            self.assertListEqual(CliParser.parse_list_arg(arg, 'main'), ['main'])
+
+        self.assertListEqual(CliParser.parse_list_arg('download'), ['download'])
+        self.assertListEqual(CliParser.parse_list_arg('download,train'), ['download', 'train'])
+
+    def test_parse_extra_args(self) -> None:
+        mlcube_args, task_args = CliParser.parse_extra_arg(
+            "-Pdocker.image=IMAGE_NAME",
+            "data_config=/configs/data.yaml",
+            "-Pplatform.host_memory_gb=30",
+            "data_dir=/data/imagenet"
+        )
+        self.assertIsInstance(mlcube_args, DictConfig)
+        self.assertEqual(
+            OmegaConf.to_container(mlcube_args),
+            {'docker': {'image': 'IMAGE_NAME'}, 'platform': {'host_memory_gb': 30}}
+        )
+
+        self.assertIsInstance(task_args, dict)
+        self.assertEqual(
+            task_args,
+            {'data_config': '/configs/data.yaml', 'data_dir': '/data/imagenet'}
+        )
