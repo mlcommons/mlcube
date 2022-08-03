f5816fd08405faf6683533673db2e2c8db58d5d2
Sergey Serebryakov <serebryakov.sergey@gmail.com>
2022-08-02 23:01:11 -0700

    New unit tests for MLCube singularity runner. (#262)
    1. New unit tests for singularity runner. Runner itself (configure/run methods) is tested if singularity is available in the system. The test pulls `ubuntu:18.04` docker image (~ 64 MB) and creates corresponding SIF file as part of test setup.
    2. `Config.merge`: if `runner` section is not present, it is automatically created.
    3. Annotating `task` parameter as optional for the SingularityRun.__init__ method.

---
+++ runners/mlcube_singularity/mlcube_singularity/tests/test_config.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+
+from omegaconf import OmegaConf
+
+from mlcube_singularity.singularity_run import Config
+
+
+class TestConfig(TestCase):
+    def test_merge(self) -> None:
+        scfg = {'image': 'mnist-0.0.1.sif', 'image_dir': '/path/to/image', 'singularity': 'singularity'}
+        config = OmegaConf.create({
+            'singularity': scfg,
+            'runtime': {'workspace': '/path/to/workspace'}
+        })
+        Config.merge(config)
+        self.assertEqual(
+            config,
+            OmegaConf.create({
+                'runner': scfg,
+                'singularity': scfg,
+                'runtime': {'workspace': '/path/to/workspace'}
+            })
+        )
+
+    def test_validate(self) -> None:
+        config = OmegaConf.create({
+            'singularity': {'image': 'mnist-0.0.1.sif'},
+            'runtime': {'workspace': '/path/to/workspace'},
+            'runner': Config.DEFAULT.copy()
+        })
+        Config.validate(config)
