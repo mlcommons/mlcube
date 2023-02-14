2fee17b0f9332811b408a18fd79cdca20bed065e
Sergey Serebryakov <serebryakov.sergey@gmail.com>
2023-02-13 16:13:53 -0800

    Moving k8s runner docs dependency to a separate file. (#296)
    The MLCube k8s runner lists `diagrams` as its dependency. This is only required
    to generate documentation (one-time action), and is not required by k8s runner to
    run user ML cubes. This commit moves this dependency to a different file.

--- runners/mlcube_k8s/mlcube_k8s/design/diagram.py
+++ runners/mlcube_k8s/mlcube_k8s/design/diagram.py
@@ -3,6 +3,9 @@ from diagrams.k8s.compute import Job
 from diagrams.onprem import client, container
 from diagrams.custom import Custom
 
+# A helper script to generate a K8S diagram for documentation. This
+# image should be copied `to PROJECT_ROOT/docs/assets/mlcube-k8s.png`.
+
 with Diagram(name="MLCube-k8s", show=False):
     with Cluster("Kubernetes Runner"):
         yaml_icon = "yaml.jpg"
